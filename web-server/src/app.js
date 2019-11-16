9b3c848e5f0a16b06f51946627fbd993745a628f
maytezhou <you@example.com>
2019-11-16 12:11:09 -0500

    Using Fetch API Client Side Javascript

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -51,7 +51,7 @@ app.get('/weather',(req,res)=>{
         return res.send({ error:"You must provide an address"}) 
   
       }
-      geocode(req.query.address,(error,{latitude,longitude,location})=>{
+      geocode(req.query.address,(error,{latitude,longitude,location}={})=>{
         if (error){
             return res.send({error})
         }
