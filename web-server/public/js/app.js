cd2f00d08b615f92544ced82eec85295755d744f
maytezhou <you@example.com>
2019-11-17 10:39:04 -0500

    fetching and console.log(weather data)

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,12 +1,6 @@
 
 console.log('CLient side javascript file is loaded')
-fetch('http://puzzle.mead.io/puzzle')
-.then((response)=> response.json().then((data)=>{
-console.log(data)
-}))
-
-
-fetch('http://localhost:3000/weather?address=!')
+/* fetch('http://localhost:3000/weather?address=!')
 .then((response)=> response.json().then((data)=>{
 if(data.error){
     console.log(data.error)
@@ -16,4 +10,24 @@ if(data.error){
     console.log(data.forecast);
 }
 
-}))
+})); */
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+
+weatherForm.addEventListener('submit',(e)=>{
+e.preventDefault();
+const location = search.value 
+    console.log(location);
+fetch( `http://localhost:3000/weather?address=${location}`)
+.then((response)=> response.json().then((data)=>{
+if(data.error){
+    console.log(data.error)
+   
+}else{  
+    console.log(data.location);
+    console.log(data.forecast);
+}
+
+}));
+
+})
