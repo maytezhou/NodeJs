bd69f5d6fbe609035e92bf162b351220d183fea3
maytezhou <you@example.com>
2019-11-30 15:20:09 -0500

    ultimos cambios

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -13,7 +13,9 @@ if(data.error){
 })); */
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
-
+const messageOne = document.querySelector('#message-1');
+const messageTwo = document.querySelector('#message-2');
+messageOne.textContent = 'From Javascript';
 weatherForm.addEventListener('submit',(e)=>{
 e.preventDefault();
 const location = search.value 
